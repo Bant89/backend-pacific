3cb772e8cf4a435cf3ddae5a35397a6c1ff0635c
Bant89 <bant8009@gmail.com>
2020-09-01 14:03:08 -0400

    minor change

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -4,7 +4,12 @@ class AuthenticationController < ApplicationController
   # skip_before_action :authorize_request, only: :authenticate
   def authenticate
     auth_token = AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
-    json_response(auth_token: auth_token)
+    user = User.find_by(email: auth_params[:email]) unless auth_token == nil?
+    response = {
+      user: user,
+      auth_token: auth_token
+    }
+    json_response(response, :ok)
   end
 
   private
